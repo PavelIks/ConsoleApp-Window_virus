1ab56c21364587dd072d88626fbc4ef39ecdea95
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-07 16:35:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace StartProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Process.Start(@"C:\Users\Pavlo\source\repos\StartProcess\bin\Debug\StartProcess");
+            }
+
+            /*foreach (Process process in Process.GetProcessesByName("StartProcess"))
+            {
+                process.Kill();
+            }*/
+        }
+    }
+}
